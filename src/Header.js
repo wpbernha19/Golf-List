b018033d6edd0526e9d53ec80811fff04198b581
Billy Bernhardt <wpbernha19@wabash.edu>
2017-05-23 15:54:53 -0400

    Add Header Component and Style

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return(
+    <header>
+      <h1>GolfList</h1>
+      <h2>So many things</h2>
+    </header>
+  )
+}
+
+export default Header
