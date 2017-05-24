a1560948f9e6e20ff0d4791f29ca2904cb9472ff
Billy Bernhardt <wpbernha19@wabash.edu>
2017-05-24 15:27:32 -0400

    Convert course to a class

--- src/Course.js
+++ src/Course.js
@@ -1,31 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ContentEditable from 'react-contenteditable'
 
 import './Course.css'
 import Actions from './Actions'
 
-const Course = ({ course, saveCourse, removeCourse }) => {
-  const updateName = (ev) => {
+class Course extends Component {
+  updateName = (ev) => {
+    const { course, saveCourse } = this.props
     course.name = ev.target.value
     saveCourse(course)
   }
 
-  return(
-    <li className="Course">
-      <input type="checkbox" value="on" />
-      <div className="details">
-          <ContentEditable
-            className="name"
-            html={course.name}
-            onChange={updateName}
-          />
-         <Actions 
-          course={course} 
-          removeCourse={removeCourse}
-        /> 
-      </div>
-    </li>
-  )
+render() {
+  const { course, removeCourse } = this.props
+    return(
+      <li className="Course">
+        <input type="checkbox" value="on" />
+        <div className="details">
+            <ContentEditable
+              className="name"
+              html={course.name}
+              onChange={this.updateName}
+
+            />
+          <Actions 
+            course={course} 
+            removeCourse={removeCourse}
+          /> 
+        </div>
+      </li>
+    )
+  }
 }
 
 export default Course
